7177d77152a937bbb6034d00bdd6be1fd959dac0
agctu <agctu46@outlook.com>
2025-04-13 12:33:02 +0800

    Add implementation
    If no bugs, the connection pool should operate correctly invoked by
    single thread

--- include/connection_pool.h
+++ include/connection_pool.h
@@ -5,37 +5,91 @@
 #include <list>
 #include <mutex>
 #include <thread>
+#include <exception>
+#include <set>
 
 class ConnectionPool {
 public:
-    struct Config {
 
+    struct wrong_operation : public std::runtime_error {
+        wrong_operation(const std::string& msg);
+    };
+    struct wrong_connection : public std::runtime_error {
+        wrong_connection();
+    };
+    struct connection_timeout : public std::runtime_error {
+        connection_timeout(unsigned int retry_count);
+    };
+    struct fatal_error : public std::runtime_error {
+        fatal_error(const std::string& msg);
+    };
+
+    struct Config {
+        size_t init_conn_num=10;
+        unsigned int max_retry_count=5;
+        time_t retry_interval_milli=1000;
     };
 
     struct Connection {
-        virtual bool connect() = 0;
+        // ping shouldn't be time consuming
         virtual bool ping() = 0;
+        // close shouldn't be time consuming
         virtual void close() = 0;
     };
 
     using Ptr=std::shared_ptr<Connection>;
 
-    class Creator {
+    struct Creator {
+
+        /* returned value expected to be available to use if not nullptr
+         * no throw
+         * return nullptr if failed to create
+         */
         virtual Ptr create() = 0;
     };
 
-    ConnectionPool(const Config& config, std::unique_ptr<Creator> creator);
+    ConnectionPool(const Config& config, std::unique_ptr<Creator>&& creator);
     ~ConnectionPool();
 
+    /*
+     * throw ConnectionPool::wrong_operation
+     */
+    void start();
+
+    /*
+     * throw ConnectionPool::wrong_operation
+     */
+    void stop();
+
+    /*
+     * throw ConnectionPool::wrong_connection
+     */
     Ptr getConn();
 
     void relConn(Ptr conn);
 
 private:
+    enum class Stage {
+        IDLE,
+        BUSY,
+    };
+
+    Stage stage;
+    Config config;
     std::list<Ptr> idle_conns;
-    std::list<Ptr> active_conns;
+    std::set<Ptr> active_conns;
     std::thread reaper;
     std::mutex conn_mutex;
+    std::unique_ptr<Creator> creator;
+
+    bool populate();
+    bool clear();
+    Ptr pickOneFromIdleList();
+    void appendToActiveList(Ptr conn);
+    // create new connection with retry
+    Ptr createNewConn();
+    bool removeFromActiveList(Ptr conn);
+    void appendToIdleList(Ptr conn);
 };
 
 #endif
---
+++ src/connection_pool.cpp
@@ -0,0 +1,129 @@
+#include "connection_pool.h"
+
+using namespace std;
+
+ConnectionPool::wrong_operation::wrong_operation(const string& msg)
+    : runtime_error(msg) {}
+
+ConnectionPool::wrong_connection::wrong_connection()
+    : runtime_error("you are release an unknown connection") {}
+
+ConnectionPool::connection_timeout::connection_timeout(unsigned int retry_count)
+    : runtime_error("failed to get connection after "+to_string(retry_count)+" retries") {}
+
+ConnectionPool::fatal_error::fatal_error(const string& msg)
+    : runtime_error(msg) {}
+
+ConnectionPool::ConnectionPool(
+    const Config& config, std::unique_ptr<Creator>&& creator
+) : config(config),creator(move(creator)) {
+    stage=Stage::IDLE;
+}
+
+ConnectionPool::~ConnectionPool() {
+    if(stage==Stage::BUSY) {
+        stop();
+    }
+}
+
+void ConnectionPool::start() {
+    if(stage==Stage::BUSY) {
+        throw wrong_operation("pool is already BUSY");
+    }
+    if(populate()) {
+        stage=Stage::BUSY;
+    } else {
+        if(!clear()) {
+            throw fatal_error("can't clear pool after failing to poplulate pool");
+        }
+    }
+}
+
+bool ConnectionPool::populate() {
+    for(size_t i=0;i<config.init_conn_num;++i) {
+        auto conn=creator->create();
+        if(conn==nullptr) {
+            return false;
+        }
+        idle_conns.push_back(conn);
+    }
+    return true;
+}
+
+void ConnectionPool::stop() {
+    if(stage==Stage::IDLE) {
+        throw wrong_operation("pool is already IDLE");
+    }
+    if(clear()) {
+        stage=Stage::IDLE;
+    }
+}
+
+bool ConnectionPool::clear() {
+    if(active_conns.size()!=0) {
+        return false;
+    }
+    for(auto i:idle_conns) {
+        i->close();
+    }
+    idle_conns.clear();
+    return true;
+}
+
+ConnectionPool::Ptr ConnectionPool::getConn() {
+    auto conn=pickOneFromIdleList();
+    if(!conn) {
+        conn=createNewConn();
+    }
+    appendToActiveList(conn);
+    return conn;
+}
+
+ConnectionPool::Ptr ConnectionPool::pickOneFromIdleList() {
+    for(auto iter=active_conns.begin();iter!=active_conns.end();++iter) {
+        auto conn=*iter;
+        if(conn->ping()) {
+            active_conns.erase(iter);
+            return conn;
+        }
+    }
+    return nullptr;
+}
+
+void ConnectionPool::appendToActiveList(Ptr conn) {
+    active_conns.insert(conn);
+}
+
+
+ConnectionPool::Ptr ConnectionPool::createNewConn() {
+    for(unsigned int i=0;i<config.max_retry_count;++i) {
+        auto ret=creator->create();
+        if(ret) {
+            return ret;
+        }
+        this_thread::sleep_for(chrono::milliseconds(config.retry_interval_milli));
+    }
+    throw connection_timeout(config.max_retry_count);
+}
+
+void ConnectionPool::relConn(Ptr conn) {
+    auto ok=removeFromActiveList(conn);
+    if(!ok) {
+        throw wrong_connection();
+    }
+    appendToIdleList(conn);
+}
+
+bool ConnectionPool::removeFromActiveList(Ptr conn) {
+    auto iter=active_conns.find(conn);
+    if(iter!=active_conns.end()){
+        active_conns.erase(iter);
+        return true;
+    }
+    return false;
+}
+
+void ConnectionPool::appendToIdleList(Ptr conn) {
+    idle_conns.push_back(conn);
+}
+
