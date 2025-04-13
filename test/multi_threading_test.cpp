698fe02914d99f5c492d852c9c361cc38c788b0c
agctu <agctu46@outlook.com>
2025-04-13 21:24:29 +0800

    Add testing for multithreading

---
+++ test/multi_threading_test.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+#include "connection_pool.h"
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+class Conn: public ConnectionPool::Connection {
+public:
+    void echoAddr() {}
+    bool ping() override  {
+        return true;
+    }
+    void close() override {}
+};
+
+using Ptr=shared_ptr<Conn>;
+
+class Creator: public ConnectionPool::Creator {
+public:
+    Creator(const string& addr) : addr(addr) {}
+    ConnectionPool::Ptr create() override {
+        auto ret=make_shared<Conn>();
+        cout<<ret<<" created"<<endl;
+        return ret;
+    }
+private:
+    string addr;
+};
+
+TEST(MultiThreaded,Simple) {
+    ConnectionPool pool{{},make_unique<Creator>("localhost")};
+    pool.start();
+    vector<thread> ths;
+    for(int i=0;i<100;++i) {
+        ths.push_back(thread([&pool,i]{
+            for(int i=0;i<1000;++i) {
+                auto conn=pool.getConn();
+                dynamic_pointer_cast<Conn>(conn)->echoAddr();
+                pool.relConn(conn);
+            }
+        }));
+    }
+    for(auto& i:ths)i.join();
+}
+
